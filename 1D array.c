6ec51f9907ed76e9b12431d75226746a3781738b
shrinivas443 <shrinivasgoudar443@gmail.com>
2025-11-19 12:07:58 +0530

    Create 1D array.c

---
+++ 1D array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5];  
+    int i;
+
+    printf("Enter 5 numbers:\n");
+    for(i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\nYou entered:\n");
+    for(i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
