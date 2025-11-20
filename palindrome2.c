139c9720123c981e4cc59e77d09f4d467bfa7abb
shrinivas443 <shrinivasgoudar443@gmail.com>
2025-11-20 12:00:46 +0530

    Create palindrome2.c

---
+++ palindrome2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[20];
+    char rev[20];
+    int len;
+    printf("enter a string");
+    scanf("%s",&str);
+     len=strlen(str);
+     for(int i=0;i<=len;i++){
+         rev[i]=str[len-1-i];
+     }
+     
+     if(strcmp(str,rev)==0){
+         printf("paldorime");
+     }
+     else{
+         printf("not paldorime");
+     }
+}
