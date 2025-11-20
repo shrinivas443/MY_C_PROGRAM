b61ad8d3dabe4621c73ddc9e0493c07cfcbae4f0
shrinivas443 <shrinivasgoudar443@gmail.com>
2025-11-20 10:27:28 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int num=121,org,rev=0,rem;
+    org=num;
+    while(num!=0){
+        rem=num%10;
+        rev=rev*10+rem;
+        num=num/10;
+    }
+    if(org==rev){
+        printf("num is pal");
+    }
+    else{
+        printf("not a pal");
+    }
+}
